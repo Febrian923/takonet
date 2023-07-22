329b218f50a1f0af5eaae7b19dced05962c7ddcf
febrian923 <ragilfebriansyah321@gmail.com>
2023-07-22 14:58:44 +0700

    editing php

--- php/register.php
+++ php/register.php
@@ -1,29 +1,15 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Ambil data yang dikirim melalui form
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirmPassword = $_POST['confirm_password'];
+require 'koneksi.php';
+$Nama = $_post["nama"];
+$Email = $_post["email"];
+$Password = $_post["password"];
+$repassword = $_post["repassword"];
 
-    // Validasi data (misalnya: cek apakah password dan konfirmasi password sama)
-    if ($password !== $confirmPassword) {
-        echo 'Konfirmasi password tidak sesuai';
-        exit;
-    }
+$query_sql = "INSERT INTO tbl_user (nama, email, password, repassword)
+            VALUES ('$nama', '$email', '$password', '$repassword')";
 
-    // Proses penyimpanan data ke database atau langkah-langkah pendaftaran lainnya
-
-    // Contoh: Menyimpan data dalam file CSV
-    $data = [
-        $name,
-        $email,
-        $password
-    ];
-    $file = fopen('data.csv', 'a');
-    fputcsv($file, $data);
-    fclose($file);
-
-    echo 'Pendaftaran berhasil!';
+if(mysqli_query($conn, $query_sql)) {
+    header("location : login.html");
+} else{
+    echo "pendaftaran gagal : " . mysqli_error($conn);
 }
-?>
